6ab71acaa102268cee4214d1ae62742ecaf5051e
Aramez <191219550+Aramez-0@users.noreply.github.com>
2025-02-10 10:43:51 +1000

    Begginings of UI and project functionality

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-
+eval("let toDo = function(title, desc, due, priority) {\n    return { title, desc, due, priority }\n}\n\nconsole.log('wassup')\n\n//  Projects\n// tab/sidebar for projects\n// create new projects\n// list all to-do's in a project\n\n\n//  To-do\n// when adding new to-do default to home project or add way to add to \n// \\ project when creating new \n// expand to-do \n// delete to-do\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6IkFBQUE7QUFDQSxhQUFhO0FBQ2I7O0FBRUE7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7OztBQUdBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvaW5kZXguanM/YjYzNSJdLCJzb3VyY2VzQ29udGVudCI6WyJsZXQgdG9EbyA9IGZ1bmN0aW9uKHRpdGxlLCBkZXNjLCBkdWUsIHByaW9yaXR5KSB7XG4gICAgcmV0dXJuIHsgdGl0bGUsIGRlc2MsIGR1ZSwgcHJpb3JpdHkgfVxufVxuXG5jb25zb2xlLmxvZygnd2Fzc3VwJylcblxuLy8gIFByb2plY3RzXG4vLyB0YWIvc2lkZWJhciBmb3IgcHJvamVjdHNcbi8vIGNyZWF0ZSBuZXcgcHJvamVjdHNcbi8vIGxpc3QgYWxsIHRvLWRvJ3MgaW4gYSBwcm9qZWN0XG5cblxuLy8gIFRvLWRvXG4vLyB3aGVuIGFkZGluZyBuZXcgdG8tZG8gZGVmYXVsdCB0byBob21lIHByb2plY3Qgb3IgYWRkIHdheSB0byBhZGQgdG8gXG4vLyBcXCBwcm9qZWN0IHdoZW4gY3JlYXRpbmcgbmV3IFxuLy8gZXhwYW5kIHRvLWRvIFxuLy8gZGVsZXRlIHRvLWRvXG5cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.js\n");
 
 /***/ })
 
